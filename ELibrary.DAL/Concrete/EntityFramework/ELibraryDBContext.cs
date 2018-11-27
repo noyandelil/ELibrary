9e42e39a6e9fb6c973916d390a94e52ba140c2bc
Ekin Cihan Duman <e.cihanduman@gmail.com>
2018-11-27 21:00:35 +0300

    First Commit
    First Commit
    
    Co-Authored-By: EkinCihanDuman <ekincihanduman@users.noreply.github.com>

---
+++ ELibrary.DAL/Concrete/EntityFramework/ELibraryDBContext.cs
@@ -0,0 +1,21 @@
+﻿using ELibrary.Entities.Concrete;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ELibrary.DAL.Concrete.EntityFramework
+{
+    public class ELibraryDBContext : IdentityDbContext<ApplicationUser> 
+    {
+        public virtual DbSet<Author> Authors { get; set; }
+        public ELibraryDBContext(DbContextOptions options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ ELibrary.Entities/Concrete/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ELibrary.Core.Entites;
+using Microsoft.AspNetCore.Identity;
+
+namespace ELibrary.Entities.Concrete
+{
+    public class ApplicationUser : IdentityUser, IEntity
+    {
+    }
+}
